14f37c24d30d1b0c90b75a770f761eaac044998f
Curtis Malainey <curtis@malainey.com>
2017-09-16 19:34:06 -0700

    Correct naming in example and add comments

--- examples/HeartRateMonitor/HeartRateMonitor.ino
+++ examples/HeartRateMonitor/HeartRateMonitor.ino
@@ -20,22 +20,24 @@
 const uint8_t NETWORK_KEY[] = {0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77}; // get this from thisisant.com
 
 AntWithCallbacks ant = AntWithCallbacks();
-AntPlusRouter router = AntRouter();
-ProfileHRMonitor hr = ProfileHRMonitor(WILDCARD_DEVICE);
+AntPlusRouter router = AntPlusRouter();
+ProfileHeartRateMonitor hr = ProfileHeartRateMonitor(WILDCARD_DEVICE);
 
 void setup() {
     Serial1.begin(BAUD_RATE);
     ant.setSerial(Serial1);
+    delay(10000);
+
     router.setDriver(ant); // never touch ant again
     router.setAntPlusNetworkKey(NETWORK_KEY);
     router.setProfile(CHANNEL_0, hr);
     // Delay after initial setup to wait for user to connect on serial
-    delay(10000);
+
     Serial.begin(BAUD_RATE);
     Serial.println("Running");
-    hr.pair();
+    hr.connect();
     // wait for pair to complete
-    while(hr.getStatus() == CHANNEL_STATUS_SEARCHING) {router.loop()};
+    while(hr.getChannelStatus() == CHANNEL_STATUS_SEARCHING) {router.loop();};
     // print channel status
     // get sensor serial number
 }
