adb92935848b6ba038151f58ca28f10d79bc453a
Curtis Malainey <curtis@malainey.com>
2017-09-18 00:05:49 -0700

    Add datapage interface
    Can't directly inherit from broadcast data as some profiles use other
    message types such as acknowledge in bike radar.

---
+++ src/BaseClasses/ANTPLUS_BaseDataPage.h
@@ -0,0 +1,9 @@
+#ifndef ANTPLUS_BASEDATAPAGE_h
+#define ANTPLUS_BASEDATAPAGE_h
+
+class BaseDataPage {
+public:
+    virtual uint8_t getDataPageNumber() = 0;
+}
+
+#endif // ANTPLUS_BASEDATAPAGE_h
--- src/Profiles/HeartRate/DataPages/Base/ANTPLUS_HeartRateBaseMainDataPage.h
+++ src/Profiles/HeartRate/DataPages/Base/ANTPLUS_HeartRateBaseMainDataPage.h
@@ -1,1 +1,12 @@
-// TODO
+#ifndef ANTPLUS_HEARTRATEBASEMAINDATAPAGE_h
+#define ANTPLUS_HEARTRATEBASEMAINDATAPAGE_h
+
+#include <BaseClasses/ANTPLUS_BaseDataPage.h>
+
+#include "ANT.h"
+
+class HeartRateBaseMainDataPage : public BaseDataPage, public BroadcastData {
+
+}
+
+#endif // ANTPLUS_HEARTRATEBASEMAINDATAPAGE_h
