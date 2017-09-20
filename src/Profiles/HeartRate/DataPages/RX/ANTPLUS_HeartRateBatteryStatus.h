3d9e45f90e117fcc1ec236d5506e350eef9643d5
Curtis Malainey <curtis@malainey.com>
2017-09-20 00:06:40 -0700

    Add heart rate battery status declarations

--- src/Profiles/HeartRate/DataPages/RX/ANTPLUS_HeartRateBatteryStatus.h
+++ src/Profiles/HeartRate/DataPages/RX/ANTPLUS_HeartRateBatteryStatus.h
@@ -1,1 +1,16 @@
-// TODO
+#ifndef ANTPLUS_HEARTRATEBATTERYSTATUS_h
+#define ANTPLUS_HEARTRATEBATTERYSTATUS_h
+
+#include <BaseClasses/ANTPLUS_BaseDataPage.h>
+
+#include "ANT.h"
+
+class HeartRateBaseMainDataPage : public HeartRateBatteryStatus {
+public:
+    uint8_t getBatteryLevel();
+    uint8_t getFractionalBatteryVoltage();
+    uint8_t getCoarseBatteryVolage();
+    uint8_t getBatteryStatus();
+}
+
+#endif // ANTPLUS_HEARTRATEBATTERYSTATUS_h
