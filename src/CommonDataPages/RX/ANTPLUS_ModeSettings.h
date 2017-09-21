209c63f92da8a89c63df01151339e0e6132382c6
Curtis Malainey <curtis@malainey.com>
2017-09-20 23:29:38 -0700

    Add ModeSettings Interface
    Needed for HeartRate profile

--- src/CommonDataPages/RX/ANTPLUS_ModeSettings.h
+++ src/CommonDataPages/RX/ANTPLUS_ModeSettings.h
@@ -1,2 +1,14 @@
+#ifndef ANTPLUS_MODESETTINGS_h
+#define ANTPLUS_MODESETTINGS_h
+
+#include <BaseClasses/ANTPLUS_BaseDataPage.h>
+
+#include "ANT.h"
+
 // Common page 76
+
+class ModeSettings : public BaseDataPage, public BroadcastData {
 // TODO
+};
+
+#endif // ANTPLUS_MODESETTINGS_h
