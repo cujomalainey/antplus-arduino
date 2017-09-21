6cc1ff9f11b4b27a3622daf45425f4e6b904f2ee
Curtis Malainey <curtis@malainey.com>
2017-09-20 23:44:58 -0700

    Declare ModeSettingMsg Interface
    TX variant of ModeSettings

--- src/CommonDataPages/TX/ANTPLUS_ModeSettingsMsg.h
+++ src/CommonDataPages/TX/ANTPLUS_ModeSettingsMsg.h
@@ -1,2 +1,14 @@
+#ifndef ANTPLUS_MODESETTINGSMSG_h
+#define ANTPLUS_MODESETTINGSMSG_h
+
+#include <BaseClasses/ANTPLUS_BaseDataPage.h>
+
+#include "ANT.h"
+
 // Common page 76
+
+class ModeSettingsMsg : public BaseDataPage, public BroadcastDataMsg {
 // TODO
+};
+
+#endif // ANTPLUS_MODESETTINGSMSG_h
