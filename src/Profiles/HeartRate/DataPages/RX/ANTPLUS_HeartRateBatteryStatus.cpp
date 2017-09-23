928dc280a1ff9a884cdffde6701bf8ec4b939199
Curtis Malainey <curtis@malainey.com>
2017-09-22 22:24:42 -0700

    Implement BatteryStatus DataPage

--- src/Profiles/HeartRate/DataPages/RX/ANTPLUS_HeartRateBatteryStatus.cpp
+++ src/Profiles/HeartRate/DataPages/RX/ANTPLUS_HeartRateBatteryStatus.cpp
@@ -1,21 +1,22 @@
 #include <Profiles/HeartRate/DataPages/RX/ANTPLUS_HeartRateBatteryStatus.h>
+#include <Profiles/HeartRate/ANTPLUS_HeartRatePrivateDefines.h>
 
 HeartRateBatteryStatus::HeartRateBatteryStatus() : HeartRateBaseMainDataPage() {
-    // TODO
+
 }
 
 uint8_t HeartRateBatteryStatus::getBatteryLevel() {
-    // TODO
+    return getData(ANTPLUS_HEARTRATE_DATAPAGE_BATTERYSTATUS_BATTERYLEVEL_BYTE);
 }
 
 uint8_t HeartRateBatteryStatus::getFractionalBatteryVoltage() {
-    // TODO
+    return getData(ANTPLUS_HEARTRATE_DATAPAGE_BATTERYSTATUS_FRACTIONALBATTERYVOLTAGE_BYTE);
 }
 
 uint8_t HeartRateBatteryStatus::getCoarseBatteryVolage() {
-    // TODO
+    return getData(ANTPLUS_HEARTRATE_DATAPAGE_BATTERYSTATUS_COARSEBATTERYVOLTAGE_BYTE) & ANTPLUS_HEARTRATE_DATAPAGE_BATTERYSTATUS_COARSEBATTERYVOLTAGE_MASK;
 }
 
 uint8_t HeartRateBatteryStatus::getBatteryStatus() {
-    // TODO
+    return (getData(ANTPLUS_HEARTRATE_DATAPAGE_BATTERYSTATUS_BATTERYSTATUS_BYTE) & ANTPLUS_HEARTRATE_DATAPAGE_BATTERYSTATUS_BATTERYSTATUS_MASK) << ANTPLUS_HEARTRATE_DATAPAGE_BATTERYSTATUS_BATTERYSTATUS_SHIFT;
 }
