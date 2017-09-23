b63b860e74ef79668b2b29848a2f7c5ea0e1f221
Curtis Malainey <curtis@malainey.com>
2017-09-22 22:01:12 -0700

    Add functionality to base datapage class

--- src/Profiles/HeartRate/DataPages/Base/ANTPLUS_HeartRateBaseMainDataPage.cpp
+++ src/Profiles/HeartRate/DataPages/Base/ANTPLUS_HeartRateBaseMainDataPage.cpp
@@ -1,25 +1,26 @@
 #include <Profiles/HeartRate/DataPages/Base/ANTPLUS_HeartRateBaseMainDataPage.h>
+#include <Profiles/HeartRate/ANTPLUS_HeartRatePrivateDefines.h>
 
 HeartRateBaseMainDataPage::HeartRateBaseMainDataPage() {
-    // TODO
+
 }
 
 uint8_t HeartRateBaseMainDataPage::getDataPageNumber() {
-    // TODO
+    return getData(ANTPLUS_HEARTRATE_DATAPAGEBASE_DATAPAGE_BYTE) & ANTPLUS_HEARTRATE_DATAPAGEBASE_DATAPAGE_MASK;
 }
 
 uint8_t HeartRateBaseMainDataPage::getPageChangeToggle() {
-    // TODO
+    return (getData(ANTPLUS_HEARTRATE_DATAPAGEBASE_TOGGLE_BYTE) & ANTPLUS_HEARTRATE_DATAPAGEBASE_TOGGLE_MASK) >> ANTPLUS_HEARTRATE_DATAPAGEBASE_TOGGLE_SHIFT;
 }
 
 uint16_t HeartRateBaseMainDataPage::getHeartBeatEventTime() {
-    // TODO
+    return (getData(ANTPLUS_HEARTRATE_DATAPAGEBASE_HEARTBEATEVENTTIMEMSB_BYTE) << ANTPLUS_HEARTRATE_DATAPAGEBASE_HEARTBEATEVENTTIMEMSB_SHIFT) | getData(ANTPLUS_HEARTRATE_DATAPAGEBASE_HEARTBEATEVENTTIMELSB_BYTE);
 }
 
 uint8_t HeartRateBaseMainDataPage::getHeartBeatCount() {
-    // TODO
+    return getData(ANTPLUS_HEARTRATE_DATAPAGEBASE_HEARTBEATCOUNT_BYTE);
 }
 
 uint8_t HeartRateBaseMainDataPage::getComputedHeartRate() {
-    // TODO
+    return getData(ANTPLUS_HEARTRATE_DATAPAGEBASE_COMPUTEDHEARTRATE_BYTE);
 }
--- src/Profiles/HeartRate/DataPages/RX/ANTPLUS_HeartRateDefault.cpp
+++ src/Profiles/HeartRate/DataPages/RX/ANTPLUS_HeartRateDefault.cpp
@@ -1,5 +1,5 @@
 #include <Profiles/HeartRate/DataPages/RX/ANTPLUS_HeartRateDefault.h>
 
 HeartRateDefault::HeartRateDefault() : HeartRateBaseMainDataPage() {
-    // TODO
+
 }
