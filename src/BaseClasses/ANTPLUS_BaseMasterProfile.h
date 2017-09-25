80861d0dff90e7bacc315c569a26c3f99e55fccb
Curtis Malainey <curtis@malainey.com>
2017-09-25 00:15:48 -0700

    Missing virtual
    Can't implement a pure virutal if it isn't virtual

--- src/BaseClasses/ANTPLUS_BaseMasterProfile.h
+++ src/BaseClasses/ANTPLUS_BaseMasterProfile.h
@@ -4,11 +4,11 @@
 class BaseMasterProfile : public BaseProfile {
 public:
     // TODO add callback structure and handler for next datapage
-private:
+protected:
     /**
      * This function, when called, will transmit the next message in the broadcast pattern
      */
-    void transmitNextDataPage() = 0;
+    virtual void transmitNextDataPage() = 0;
 };
 
 #endif // ANTPLUS_BASESLAVEPROFILE_h
