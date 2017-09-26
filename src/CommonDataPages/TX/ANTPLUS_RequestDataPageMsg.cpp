c1db932387c322aabf4b673e48d5ea9afd5d99f0
Curtis Malainey <curtis@malainey.com>
2017-09-25 18:09:04 -0700

    Add RequestDataPage stubs

--- src/CommonDataPages/TX/ANTPLUS_RequestDataPageMsg.cpp
+++ src/CommonDataPages/TX/ANTPLUS_RequestDataPageMsg.cpp
@@ -1,1 +1,53 @@
-// TODO
+#include <CommonDataPages/TX/ANTPLUS_RequestDataPageMsg.h>
+
+RequestDataPageMsg::RequestDataPageMsg() : BaseDataPageMsg() {
+    // TODO
+}
+
+uint16_t RequestDataPageMsg::getSlaveSerialNumber() {
+    // TODO
+}
+
+uint8_t RequestDataPageMsg::getDescriptorByte1() {
+    // TODO
+}
+
+uint8_t RequestDataPageMsg::getDescriptorByte2() {
+    // TODO
+}
+
+uint8_t RequestDataPageMsg::getRequestedTransmissionResponse() {
+    // TODO
+}
+
+uint8_t RequestDataPageMsg::getRequestedPageNumber() {
+    // TODO
+}
+
+uint8_t RequestDataPageMsg::getCommandType() {
+    // TODO
+}
+
+void RequestDataPageMsg::getSlaveSerialNumber(uint16_t serialNumber) {
+    // TODO
+}
+
+void RequestDataPageMsg::getDescriptorByte1(uint8_t descriptorByte) {
+    // TODO
+}
+
+void RequestDataPageMsg::getDescriptorByte2(uint8_t descriptorByte) {
+    // TODO
+}
+
+void RequestDataPageMsg::getRequestedTransmissionResponse(uint8_t transmissionResponse) {
+    // TODO
+}
+
+void RequestDataPageMsg::getRequestedPageNumber(uint8_t pageNumber) {
+    // TODO
+}
+
+void RequestDataPageMsg::getCommandType(uint8_t commandType) {
+    // TODO
+}
--- src/CommonDataPages/TX/ANTPLUS_RequestDataPageMsg.h
+++ src/CommonDataPages/TX/ANTPLUS_RequestDataPageMsg.h
@@ -1,2 +1,27 @@
+#ifndef ANTPLUS_REQUESTDATAPAGEMSG_h
+#define ANTPLUS_REQUESTDATAPAGEMSG_h
+
+#include <BaseClasses/ANTPLUS_BaseDataPageMsg.h>
+
+#include "ANT.h"
+
 // Common Page 70
-// TODO
+
+class RequestDataPageMsg : public BaseDataPageMsg<AcknowledgedDataMsg> {
+public:
+    RequestDataPageMsg();
+    uint16_t getSlaveSerialNumber();
+    uint8_t getDescriptorByte1();
+    uint8_t getDescriptorByte2();
+    uint8_t getRequestedTransmissionResponse();
+    uint8_t getRequestedPageNumber();
+    uint8_t getCommandType();
+    void getSlaveSerialNumber(uint16_t serialNumber);
+    void getDescriptorByte1(uint8_t descriptorByte);
+    void getDescriptorByte2(uint8_t descriptorByte);
+    void getRequestedTransmissionResponse(uint8_t transmissionResponse);
+    void getRequestedPageNumber(uint8_t pageNumber);
+    void getCommandType(uint8_t commandType);
+};
+
+#endif // ANTPLUS_REQUESTDATAPAGEMSG_h
