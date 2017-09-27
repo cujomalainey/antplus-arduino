b0cdc3cd85b9c052bb3a0b9561644ec364036221
Curtis Malainey <curtis@malainey.com>
2017-09-26 18:16:27 -0700

    Add in master profile transmission handler

--- src/BaseClasses/ANTPLUS_BaseMasterProfile.cpp
+++ src/BaseClasses/ANTPLUS_BaseMasterProfile.cpp
@@ -1,1 +1,12 @@
-// TODO
+#include <BaseClasses/ANTPLUS_BaseMasterProfile.h>
+
+BaseMasterProfile::BaseMasterProfile(uint16_t deviceNumber) : BaseProfile(deviceNumber) {
+
+}
+
+void BaseMasterProfile::onChannelEventResponse(ChannelEventResponse& msg) {
+    if (msg.getCode() == STATUS_EVENT_TX) {
+        transmitNextDataPage();
+    }
+    BaseProfile::onChannelEventResponse(msg);
+}
--- src/BaseClasses/ANTPLUS_BaseMasterProfile.h
+++ src/BaseClasses/ANTPLUS_BaseMasterProfile.h
@@ -1,9 +1,18 @@
 #ifndef ANTPLUS_BASEMASTERPROFILE_h
 #define ANTPLUS_BASEMASTERPROFILE_h
 
+#include <BaseClasses/ANTPLUS_BaseProfile.h>
+
 class BaseMasterProfile : public BaseProfile {
 public:
-    // TODO add callback structure and handler for next datapage
+    BaseMasterProfile(uint16_t deviceNumber);
+    /******************************************
+     *LIBRARY INTERNAL ONLY FUNCTIONS BELOW
+     ******************************************/
+    /**
+     * Used to catch EVENT_TX and continue transmission pattern
+     */
+    virtual void onChannelEventResponse(ChannelEventResponse& msg);
 protected:
     /**
      * This function, when called, will transmit the next message in the broadcast pattern
