84f93b4c4786e633a71d8d2a4afac1ef2dd62034
Curtis Malainey <curtis@malainey.com>
2017-09-26 20:46:40 -0700

    HeartRate Manufacturer Information definitions

--- src/Profiles/HeartRate/DataPages/RX/ANTPLUS_HeartRateManufacturerInformation.cpp
+++ src/Profiles/HeartRate/DataPages/RX/ANTPLUS_HeartRateManufacturerInformation.cpp
@@ -1,13 +1,14 @@
 #include <Profiles/HeartRate/DataPages/RX/ANTPLUS_HeartRateManufacturerInformation.h>
+#include <Profiles/HeartRate/ANTPLUS_HeartRatePrivateDefines.h>
 
 HeartRateManufacturerInformation::HeartRateManufacturerInformation(AntRxDataResponse& dp) : HeartRateBaseMainDataPage(dp) {
 
 }
 
 uint8_t HeartRateManufacturerInformation::getManufacturerIdLsb() {
-    // TODO
+    return getData(ANTPLUS_HEARTRATE_DATAPAGE_MANUFACTURERINFORMATION_MANUFACTUREREIDLSB_BYTE);
 }
 
 uint16_t HeartRateManufacturerInformation::getSerialNumber() {
-    // TODO
+    return getData(ANTPLUS_HEARTRATE_DATAPAGE_MANUFACTURERINFORMATION_SERIALNUMBERLSB_BYTE) | (getData(ANTPLUS_HEARTRATE_DATAPAGE_MANUFACTURERINFORMATION_SERIALNUMBERMSB_BYTE) << ANTPLUS_HEARTRATE_DATAPAGE_MANUFACTURERINFORMATION_SERIALNUMBERMSB_SHIFT);
 }
