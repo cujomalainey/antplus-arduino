32896957a8f9b455a39c04cb3fac75217e773daa
Curtis Malainey <curtis@malainey.com>
2017-09-26 18:55:52 -0700

    Implement sports mode and relevant data

--- src/CommonDataPages/RX/ANTPLUS_ModeSettings.cpp
+++ src/CommonDataPages/RX/ANTPLUS_ModeSettings.cpp
@@ -1,9 +1,10 @@
 #include <CommonDataPages/RX/ANTPLUS_ModeSettings.h>
+#include <CommonDataPages/ANTPLUS_CommonDataPageDefines.h>
 
 ModeSettings::ModeSettings(BroadcastData& dp) : BaseDataPage<BroadcastData>(dp) {
-    // TODO
+
 }
 
 uint8_t ModeSettings::getSportMode() {
-    // TODO
+    return getData(ANTPLUS_COMMON_DATAPAGE_MODESETTINGS_SPORTSMODE_BYTE);
 }
