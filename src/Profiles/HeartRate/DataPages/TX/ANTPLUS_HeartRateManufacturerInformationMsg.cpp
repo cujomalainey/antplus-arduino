eed4cbf4987cc0e33da7232119d638f5d52f6355
Curtis Malainey <curtis@malainey.com>
2017-09-27 19:25:23 -0700

    Add manufacturerInfo message

--- src/Profiles/HeartRate/DataPages/TX/ANTPLUS_HeartRateManufacturerInformationMsg.cpp
+++ src/Profiles/HeartRate/DataPages/TX/ANTPLUS_HeartRateManufacturerInformationMsg.cpp
@@ -1,22 +1,24 @@
 #include <Profiles/HeartRate/DataPages/TX/ANTPLUS_HeartRateManufacturerInformationMsg.h>
 #include <Profiles/HeartRate/ANTPLUS_HeartRatePrivateDefines.h>
+#include <ANTPLUS_PrivateDefines.h>
 
 HeartRateManufacturerInformationMsg::HeartRateManufacturerInformationMsg() : HeartRateBaseMainDataPageMsg(ANTPLUS_HEARTRATE_DATAPAGE_MANUFACTURERINFORMATION_NUMBER) {
 
 }
 
 uint8_t HeartRateManufacturerInformationMsg::getManufacturerIdLsb() {
-    // TODO
+    return _buffer[ANTPLUS_HEARTRATE_DATAPAGE_MANUFACTURERINFORMATION_MANUFACTUREREIDLSB_BYTE];
 }
 
 uint16_t HeartRateManufacturerInformationMsg::getSerialNumber() {
-    // TODO
+    return _buffer[ANTPLUS_HEARTRATE_DATAPAGE_MANUFACTURERINFORMATION_SERIALNUMBERLSB_BYTE] | (_buffer[ANTPLUS_HEARTRATE_DATAPAGE_MANUFACTURERINFORMATION_SERIALNUMBERMSB_BYTE] << ANTPLUS_HEARTRATE_DATAPAGE_MANUFACTURERINFORMATION_SERIALNUMBERMSB_SHIFT);
 }
 
 void HeartRateManufacturerInformationMsg::setManufacturerIdLsb(uint8_t id) {
-    // TODO
+    _buffer[ANTPLUS_HEARTRATE_DATAPAGE_MANUFACTURERINFORMATION_MANUFACTUREREIDLSB_BYTE] = id;
 }
 
 void HeartRateManufacturerInformationMsg::setSerialNumber(uint16_t serialNumber) {
-    // TODO
+    _buffer[ANTPLUS_HEARTRATE_DATAPAGE_MANUFACTURERINFORMATION_SERIALNUMBERLSB_BYTE] = serialNumber & ANTPLUS_DEFAULT_BYTE_MASK;
+    _buffer[ANTPLUS_HEARTRATE_DATAPAGE_MANUFACTURERINFORMATION_SERIALNUMBERMSB_BYTE] = serialNumber >> ANTPLUS_HEARTRATE_DATAPAGE_MANUFACTURERINFORMATION_SERIALNUMBERMSB_SHIFT;
 }
