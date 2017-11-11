d270c1183d63bc7844ddbd62486431cb4bd1b2c1
Curtis Malainey <curtis@malainey.com>
2017-11-11 08:46:15 -0800

    Initial commit of environment profile
    Add skeleton class and initial messages

---
+++ src/Profiles/Environment/Display/ANTPLUS_ProfileEnvironmentDisplay.cpp
@@ -0,0 +1,41 @@
+#include <Profiles/HeartRate/Monitor/ANTPLUS_ProfileEnvironmentDisplay.h>
+#include <Profiles/HeartRate/ANTPLUS_EnvironmentPrivateDefines.h>
+#include <CommonDataPages/ANTPLUS_CommonDataPageDefines.h>
+
+ProfileEnvironmentDisplay::ProfileEnvironmentDisplay() : BaseSlaveProfile() {
+    setChannelConfig();
+}
+
+ProfileEnvironmentDisplay::ProfileEnvironmentDisplay(uint16_t deviceNumber) : BaseSlaveProfile(deviceNumber) {
+    setChannelConfig();
+}
+
+ProfileEnvironmentDisplay::ProfileEnvironmentDisplay(uint16_t deviceNumber, uint8_t transmissionType) : BaseSlaveProfile(deviceNumber, transmissionType) {
+    setChannelConfig();
+}
+
+void ProfileEnvironmentDisplay::onBroadcastData(BroadcastData& msg) {
+    EnvironmentBaseMainDataPage dp = EnvironmentBaseMainDataPage(msg);
+    uint8_t dataPage = dp.getDataPageNumber();
+    bool called = false;
+
+    BaseProfile::onBroadcastData(msg);
+
+    switch (dataPage) {
+    }
+
+    if (!called) {
+        callOnOtherDataPage(msg);
+    }
+}
+
+void ProfileEnvironmentDisplay::onAcknowledgedData(AcknowledgedData& msg) {
+    // TODO
+}
+
+void ProfileHeartRateMonitor::setChannelConfig() {
+    setChannelType(ANTPLUS_ENVIRONMENT_CHANNELTYPE);
+    setDeviceType(ANTPLUS_ENVIRONMENT_DEVICETYPE);
+    setChannelPeriod(ANTPLUS_ENVIRONMENT_CHANNELPERIOD);
+    setSearchTimeout(ANTPLUS_ENVIRONMENT_SEARCHTIMEOUT);
+}
---
+++ src/Profiles/Environment/Display/ANTPLUS_ProfileEnvironmentDisplay.h
@@ -0,0 +1,29 @@
+#ifndef ANTPLUS_PROFILEHEARTRATEMONITOR_h
+#define ANTPLUS_PROFILEHEARTRATEMONITOR_h
+
+#include <BaseClasses/ANTPLUS_BaseSlaveProfile.h>
+#include <Profiles/Environment/DataPages/ANTPLUS_ProfileEnvironmentDataPages.h>
+#include <CommonDataPages/ANTPLUS_CommonDataPages.h>
+
+#include "ANT.h"
+
+class ProfileEnvironmentDisplay : public BaseSlaveProfile {
+public:
+    ProfileEnvironmentDisplay();
+    ProfileEnvironmentDisplay(uint16_t deviceNumber);
+    ProfileEnvironmentDisplay(uint16_t deviceNumber, uint8_t transmissionType);
+    void onEnvironmentGeneralInformation(void (*func)(EnvironmentGeneralInformation&, uintptr_t), uintptr_t data = 0) { _onEnvironmentGeneralInformation.set(func, data); }
+
+    void begin();
+    void stop();
+
+private:
+    void setChannelConfig();
+    void onBroadcastData(BroadcastData& msg);
+    void onAcknowledgedData(AcknowledgedData& msg);
+
+    bool handleGeneralInformation(EnvironmentGeneralInformation& dataPage);
+    Callback<EnvironmentGeneralInformation&> _onEnvironmentGeneralInformation;
+};
+
+#endif // ANTPLUS_PROFILEHEARTRATEMONITOR_h
