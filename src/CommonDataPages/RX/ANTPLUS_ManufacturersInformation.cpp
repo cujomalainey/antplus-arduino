d72a47bd0e8d1fe1609abe3613b7738ae30458bc
Curtis Malainey <curtis@malainey.com>
2017-11-12 23:08:47 -0800

    Add Manufacturers Information stub

--- src/CommonDataPages/RX/ANTPLUS_ManufacturersInformation.cpp
+++ src/CommonDataPages/RX/ANTPLUS_ManufacturersInformation.cpp
@@ -1,1 +1,8 @@
+#include <CommonDataPages/RX/ANTPLUS_ManufacturersInformation.h>
+#include <CommonDataPages/ANTPLUS_CommonDataPageDefines.h>
+
+ManufacturersInformation::ManufacturersInformation(BroadcastData& dp) : BaseDataPage<BroadcastData>(dp) {
+
+}
+
 // TODO
