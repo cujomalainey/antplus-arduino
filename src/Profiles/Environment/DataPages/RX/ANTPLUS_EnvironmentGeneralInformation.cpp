fdf583c8ce9dcec09934419be1b73edea278b41d
Curtis Malainey <curtis@malainey.com>
2017-11-15 22:42:44 -0800

    Add GeneralInformation method stubs

--- src/Profiles/Environment/DataPages/RX/ANTPLUS_EnvironmentGeneralInformation.cpp
+++ src/Profiles/Environment/DataPages/RX/ANTPLUS_EnvironmentGeneralInformation.cpp
@@ -1,1 +1,20 @@
 #include <Profiles/Environment/DataPages/RX/ANTPLUS_EnvironmentGeneralInformation.h>
+
+EnvironmentGeneralInformation::EnvironmentGeneralInformation(AntRxDataResponse& dp) : EnvironmentBaseDataPage(dp) {
+}
+
+uint8_t EnvironmentGeneralInformation::getTransmissionInfoLocalTime() {
+    // TODO
+}
+
+uint8_t EnvironmentGeneralInformation::getTransmissionInfoUtcTime() {
+    // TODO
+}
+
+uint8_t EnvironmentGeneralInformation::getTransmissionInfoDefaultTransmissionRate() {
+    // TODO
+}
+
+uint32_t EnvironmentGeneralInformation::getSupportedPages() {
+    // TODO
+}
--- src/Profiles/Environment/DataPages/RX/ANTPLUS_EnvironmentGeneralInformation.h
+++ src/Profiles/Environment/DataPages/RX/ANTPLUS_EnvironmentGeneralInformation.h
@@ -6,6 +6,10 @@
 class EnvironmentGeneralInformation : public EnvironmentBaseDataPage {
 public:
     EnvironmentGeneralInformation(AntRxDataResponse& dp);
+    uint8_t getTransmissionInfoLocalTime();
+    uint8_t getTransmissionInfoUtcTime();
+    uint8_t getTransmissionInfoDefaultTransmissionRate();
+    uint32_t getSupportedPages();
 };
 
 #endif // ANTPLUS_ENVIRONMENTGENERALINFORMATION_h
