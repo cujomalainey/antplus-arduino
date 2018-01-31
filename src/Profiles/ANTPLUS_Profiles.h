c83a38fca253a0d42ef63c36a184e9593b1c50d8
Curtis Malainey <curtis@malainey.com>
2018-01-31 08:45:41 -0700

    Initial commit of Bicycle Speed Profile

--- src/Profiles/ANTPLUS_Profiles.h
+++ src/Profiles/ANTPLUS_Profiles.h
@@ -1,6 +1,7 @@
 #ifndef ANTPLUS_PROFILES_h
 #define ANTPLUS_PROFILES_h
 
+#include <Profiles/BicycleSpeed/ANTPLUS_BicycleSpeedProfile.h>
 #include <Profiles/HeartRate/ANTPLUS_HeartRateProfile.h>
 
 #endif // ANTPLUS_ANTROUTER_h
---
+++ src/Profiles/BicycleSpeed/Sensor/ANTPLUS_ProfileBicycleSpeedSensor.h
@@ -0,0 +1,28 @@
+#ifndef ANTPLUS_PROFILEHEARTRATESENSOR_h
+#define ANTPLUS_PROFILEHEARTRATESENSOR_h
+
+#include <BaseClasses/ANTPLUS_BaseMasterProfile.h>
+#include <Profiles/HeartRate/DataPages/ANTPLUS_ProfileHeartRateDataPages.h>
+#include <CommonDataPages/ANTPLUS_CommonDataPages.h>
+
+class ProfileHeartRateMonitor : BaseMasterProfile {
+public:
+    void onHeartRateCapabilities(void (*func)(HeartRateCapabilities&, uintptr_t), uintptr_t data = 0) { _onHeartRateCapabilities.set(func, data); }
+    void onRequestDataPage(void (*func)(RequestDataPage&, uintptr_t), uintptr_t data = 0) { _onRequestDataPage.set(func, data); }
+    void begin();
+    void stop();
+    /******************************************
+     *LIBRARY INTERNAL ONLY FUNCTIONS BELOW
+     ******************************************/
+    void onAcknowledgedData(AcknowledgedData& msg);
+    void onBroadcastData(BroadcastData& msg);
+protected:
+    void transmitNextDataPage();
+private:
+    bool handleCapabilities(HeartRateBaseMainDataPage& dataPage);
+    bool handleRequestDataPage(HeartRateBaseMainDataPage& dataPage);
+    Callback<HeartRateCapabilities&> _onHeartRateCapabilities;
+    Callback<RequestDataPage&> _onRequestDataPage;
+};
+
+#endif // ANTPLUS_PROFILEHEARTRATESENSOR_h
