54a8455a1b2e383d42ee4a3eefecb60a8ea49314
Curtis Malainey <curtis@malainey.com>
2018-04-29 20:55:40 -0600

    Define some more headers regardin slave search

--- src/BaseClasses/ANTPLUS_BaseSlaveProfile.h
+++ src/BaseClasses/ANTPLUS_BaseSlaveProfile.h
@@ -11,10 +11,41 @@ public:
 
     /**
      * Pass in a pointer to an array uint16_t[ANTPLUS_MAX_CHANNELS_POSSIBLE]
+     * Starts a search for all devices configured with this profle
+     * This will disrupt all other channels to do the search, therefore you will have to
+     * call start() on all registered profiles.
+     * This is a blocking method, see backgroundDeviceSearch for a non-blocking variation.
      *
-     * Any non-0 entry after the function returned is a nearby device
+     * returns the number of the ids filled.
      */
     uint8_t searchForDevices(uint16_t* buffer);
+
+    /**
+     * Pass in a pointer to an array uint16_t[ANTPLUS_MAX_CHANNELS_POSSIBLE]
+     * Start a non-blocking search for the following device profile.
+     * This will disrupt all other channels to do the search, therefore you will have to
+     * call start() on all registered profiles.
+     */
+    void backgroundDeviceSearch(uint16_t* buffer);
+
+    /**
+     * Check the status of the backgroundDeviceSearch.
+     */
+    uint8_t backgroundDeviceSearchStatus();
+
+    /**
+     * Call this to determine the number of devices found using backgroundDeviceSearch.
+     * This can be called before the search is completed.
+     */
+    uint8_t backgroundDeviceSearchNumberDevices();
+
+    /**
+     * TODO
+     *
+     * Register a callback method for when backgroundDeviceSearch completes.
+     */
+    void backgroundDeviceSearchCallback();
+
     /**
      * Will return once the device either pairs or timesout its search
      */
