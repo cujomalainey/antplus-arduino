078eec1feaed8304f5ab60c9535634f647956e8d
Curtis Malainey <curtis@malainey.com>
2018-05-02 01:17:52 -0600

    Add a pile of code to help with message generation and broadcast pattern

--- src/Profiles/HeartRate/Monitor/ANTPLUS_ProfileHeartRateMonitor.cpp
+++ src/Profiles/HeartRate/Monitor/ANTPLUS_ProfileHeartRateMonitor.cpp
@@ -2,6 +2,9 @@
 #include <Profiles/HeartRate/ANTPLUS_HeartRatePrivateDefines.h>
 #include <CommonDataPages/ANTPLUS_CommonDataPagePrivateDefines.h>
 
+ProfileHeartRateMonitor::ProfileHeartRateMonitor(uint16_t deviceNumber, uint8_t transmissionType) : BaseMasterProfile(deviceNumber, transmissionType), _nextBackgroundPage(ANTPLUS_HEARTRATE_DATAPAGE_MANUFACTURERINFORMATION_NUMBER) {
+}
+
 void ProfileHeartRateMonitor::onBroadcastData(BroadcastData& msg) {
     HeartRateBaseMainDataPage dp = HeartRateBaseMainDataPage(msg);
     uint8_t dataPage = dp.getDataPageNumber();
@@ -10,10 +13,7 @@ void ProfileHeartRateMonitor::onBroadcastData(BroadcastData& msg) {
     BaseProfile::onBroadcastData(msg);
 
     switch (dataPage) {
-
-    case ANTPLUS_COMMON_DATAPAGE_MODESETTINGS_NUMBER:
-        called = handleModeSettings(dp);
-        break;
+        // Display always should be using acknowledged messages
     }
 
     if (!called) {
@@ -33,6 +33,9 @@ void ProfileHeartRateMonitor::onAcknowledgedData(AcknowledgedData& msg) {
     case ANTPLUS_COMMON_DATAPAGE_REQUESTDATAPAGE_NUMBER:
         called = handleRequestDataPage(dp);
         break;
+    case ANTPLUS_COMMON_DATAPAGE_MODESETTINGS_NUMBER:
+        called = handleModeSettings(dp);
+        break;
     }
 
     if (!called) {
@@ -43,24 +46,138 @@ void ProfileHeartRateMonitor::onAcknowledgedData(AcknowledgedData& msg) {
 void ProfileHeartRateMonitor::transmitNextDataPage() {
     static uint8_t patternStep = 0;
     if (_requestedCount > 0) {
-        if (patternStep++ < 65) {
+        if (patternStep++ < 64) {
             transmitPrimaryDataPage();
         } else {
             transmitBackgroundDataPage();
-            patternStep = 0;
+            if (patternStep > 67) {
+                patternStep = 0;
+            }
         }
     } else {
         transmitRequestedDataPage();
+        _requestedCount--;
     }
 }
 
 void ProfileHeartRateMonitor::transmitPrimaryDataPage() {
+    if (_sportsMode == ANTPLUS_COMMON_DATAPAGE_MODESETTINGS_SPORTSMODE_SWIMMING) {
+        transmitHeartRateSwimIntervalSummaryMsg();
+    } else {
+        if (_useDefaultDataPage) {
+            transmitHeartRateDefaultMsg();
+        } else {
+            transmitHeartRatePreviousHeartBeatMsg();
+        }
+    }
 }
 
 void ProfileHeartRateMonitor::transmitBackgroundDataPage() {
+    switch (_nextBackgroundPage) {
+    case ANTPLUS_HEARTRATE_DATAPAGE_CUMULATIVEOPERATINGTIME_NUMBER:
+        transmitHeartRateCumulativeOperatingTimeMsg();
+        break;
+    case ANTPLUS_HEARTRATE_DATAPAGE_MANUFACTURERINFORMATION_NUMBER:
+        transmitHeartRateManufacturerInformationMsg();
+        break;
+    case ANTPLUS_HEARTRATE_DATAPAGE_PRODUCTINFORMATION_NUMBER:
+        transmitHeartRateProductInformationMsg();
+        break;
+    }
+
+    if (_requestedCount == 68) {
+        _nextBackgroundPage++;
+        if (_nextBackgroundPage == 4) {
+            if (_useCumulativeOperatingTime) {
+                _nextBackgroundPage = ANTPLUS_HEARTRATE_DATAPAGE_CUMULATIVEOPERATINGTIME_NUMBER;
+            } else {
+                _nextBackgroundPage = ANTPLUS_HEARTRATE_DATAPAGE_MANUFACTURERINFORMATION_NUMBER;
+            }
+        }
+    }
 }
 
 void ProfileHeartRateMonitor::transmitRequestedDataPage() {
+    _requestedCount--;
+    switch (_requestedPage) {
+    case ANTPLUS_HEARTRATE_DATAPAGE_DEFAULT_NUMBER:
+        transmitHeartRateDefaultMsg();
+        break;
+    case ANTPLUS_HEARTRATE_DATAPAGE_CUMULATIVEOPERATINGTIME_NUMBER:
+        transmitHeartRateCumulativeOperatingTimeMsg();
+        break;
+    case ANTPLUS_HEARTRATE_DATAPAGE_MANUFACTURERINFORMATION_NUMBER:
+        transmitHeartRateManufacturerInformationMsg();
+        break;
+    case ANTPLUS_HEARTRATE_DATAPAGE_PRODUCTINFORMATION_NUMBER:
+        transmitHeartRateProductInformationMsg();
+        break;
+    case ANTPLUS_HEARTRATE_DATAPAGE_PREVIOUSHEARTBEAT_NUMBER:
+        transmitHeartRatePreviousHeartBeatMsg();
+        break;
+    case ANTPLUS_HEARTRATE_DATAPAGE_SWIMINTERVALSUMMARY_NUMBER:
+        transmitHeartRateSwimIntervalSummaryMsg();
+        break;
+    case ANTPLUS_HEARTRATE_DATAPAGE_CAPABILITIES_NUMBER:
+        transmitHeartRateCapabilitiesMsg();
+        break;
+    case ANTPLUS_HEARTRATE_DATAPAGE_BATTERYSTATUS_NUMBER:
+        transmitHeartRateBatteryStatusMsg();
+        break;
+    }
+}
+
+void ProfileHeartRateMonitor::transmitHeartRateDefaultMsg() {
+    HeartRateDefaultMsg msg;
+    _createHeartRateDefaultMsg.call(msg);
+    transmitHeartRateMsg(msg);
+}
+
+void ProfileHeartRateMonitor::transmitHeartRateCumulativeOperatingTimeMsg() {
+    HeartRateCumulativeOperatingTimeMsg msg;
+    _createHeartRateCumulativeOperatingTimeMsg.call(msg);
+    transmitHeartRateMsg(msg);
+}
+
+void ProfileHeartRateMonitor::transmitHeartRateManufacturerInformationMsg() {
+    HeartRateManufacturerInformationMsg msg;
+    _createHeartRateManufacturerInformationMsg.call(msg);
+    transmitHeartRateMsg(msg);
+}
+
+void ProfileHeartRateMonitor::transmitHeartRateProductInformationMsg() {
+    HeartRateProductInformationMsg msg;
+    _createHeartRateProductInformationMsg.call(msg);
+    transmitHeartRateMsg(msg);
+}
+
+void ProfileHeartRateMonitor::transmitHeartRatePreviousHeartBeatMsg() {
+    HeartRatePreviousHeartBeatMsg msg;
+    _createHeartRatePreviousHeartBeatMsg.call(msg);
+    transmitHeartRateMsg(msg);
+}
+
+void ProfileHeartRateMonitor::transmitHeartRateSwimIntervalSummaryMsg() {
+    HeartRateSwimIntervalSummaryMsg msg;
+    _createHeartRateSwimIntervalSummaryMsg.call(msg);
+    transmitHeartRateMsg(msg);
+}
+
+void ProfileHeartRateMonitor::transmitHeartRateCapabilitiesMsg() {
+    HeartRateCapabilitiesMsg msg;
+    _createHeartRateCapabilitiesMsg.call(msg);
+    transmitHeartRateMsg(msg);
+}
+
+void ProfileHeartRateMonitor::transmitHeartRateBatteryStatusMsg() {
+    HeartRateBatteryStatusMsg msg;
+    _createHeartRateBatteryStatusMsg.call(msg);
+    transmitHeartRateMsg(msg);
+}
+
+void ProfileHeartRateMonitor::transmitHeartRateMsg(HeartRateBaseMainDataPageMsg& msg) {
+    // TODO set page toggle
+    send(msg);
 }
 
 bool ProfileHeartRateMonitor::handleModeSettings(HeartRateBaseMainDataPage& dataPage) {
@@ -70,6 +187,11 @@ bool ProfileHeartRateMonitor::handleModeSettings(HeartRateBaseMainDataPage& data
 
 bool ProfileHeartRateMonitor::handleRequestDataPage(HeartRateBaseMainDataPage& dataPage) {
     RequestDataPage dp(dataPage);
-    // TODO handle requested data and populate members
+    // NOTE, according to the HR profile, we should ignore the use acknowledged bit and use
+    // only broadcast messages which makes handling requests easier
+    if (dp.getRequestedPageNumber() < 8 && (dp.getRequestedPageNumber() != ANTPLUS_HEARTRATE_DATAPAGE_SWIMINTERVALSUMMARY_NUMBER || _sportsMode == ANTPLUS_COMMON_DATAPAGE_MODESETTINGS_SPORTSMODE_SWIMMING)) {
+        _requestedCount = dp.getRequestedPageCount();
+        _requestedPage = dp.getRequestedPageNumber();
+    }
     return _onRequestDataPage.call(dp);
 }
--- src/Profiles/HeartRate/Monitor/ANTPLUS_ProfileHeartRateMonitor.h
+++ src/Profiles/HeartRate/Monitor/ANTPLUS_ProfileHeartRateMonitor.h
@@ -4,14 +4,24 @@
 #include <BaseClasses/ANTPLUS_BaseMasterProfile.h>
 #include <Profiles/HeartRate/DataPages/ANTPLUS_ProfileHeartRateDataPages.h>
 #include <CommonDataPages/ANTPLUS_CommonDataPages.h>
+#include <CommonDataPages/ANTPLUS_CommonDataPageDefines.h>
 
 class ProfileHeartRateMonitor : BaseMasterProfile {
 public:
-    void createHeartRateDefault(void (*func)(HeartRateDefault&, uintptr_t), uintptr_t data = 0) { _createHeartRateDefault.set(func, data); }
-    void createHeartRateCapabilities(void (*func)(HeartRateCapabilities&, uintptr_t), uintptr_t data = 0) { _createHeartRateCapabilities.set(func, data); }
-    void createHeartRateBatteryStatus(void (*func)(HeartRateBatteryStatus&, uintptr_t), uintptr_t data = 0) { _createHeartRateBatteryStatus.set(func, data); }
+    ProfileHeartRateMonitor(uint16_t deviceNumber, uint8_t transmissionType);
+    void createHeartRateDefaultMsg(void (*func)(HeartRateDefaultMsg&, uintptr_t), uintptr_t data = 0) { _createHeartRateDefaultMsg.set(func, data); }
+    void createHeartRateCumulativeOperatingTimeMsg(void (*func)(HeartRateCumulativeOperatingTimeMsg&, uintptr_t), uintptr_t data = 0) { _createHeartRateCumulativeOperatingTimeMsg.set(func, data); }
+    void createHeartRateManufacturerInformationMsg(void (*func)(HeartRateManufacturerInformationMsg&, uintptr_t), uintptr_t data = 0) { _createHeartRateManufacturerInformationMsg.set(func, data); }
+    void createHeartRateProductInformationMsg(void (*func)(HeartRateProductInformationMsg&, uintptr_t), uintptr_t data = 0) { _createHeartRateProductInformationMsg.set(func, data); }
+    void createHeartRatePreviousHeartBeatMsg(void (*func)(HeartRatePreviousHeartBeatMsg&, uintptr_t), uintptr_t data = 0) { _createHeartRatePreviousHeartBeatMsg.set(func, data); }
+    void createHeartRateSwimIntervalSummaryMsg(void (*func)(HeartRateSwimIntervalSummaryMsg&, uintptr_t), uintptr_t data = 0) { _createHeartRateSwimIntervalSummaryMsg.set(func, data); }
+    void createHeartRateCapabilitiesMsg(void (*func)(HeartRateCapabilitiesMsg&, uintptr_t), uintptr_t data = 0) { _createHeartRateCapabilitiesMsg.set(func, data); }
+    void createHeartRateBatteryStatusMsg(void (*func)(HeartRateBatteryStatusMsg&, uintptr_t), uintptr_t data = 0) { _createHeartRateBatteryStatusMsg.set(func, data); }
     void onRequestDataPage(void (*func)(RequestDataPage&, uintptr_t), uintptr_t data = 0) { _onRequestDataPage.set(func, data); }
     void onModeSettings(void (*func)(ModeSettings&, uintptr_t), uintptr_t data = 0) { _onModeSettings.set(func, data); }
+    void setSportsMode(uint8_t mode) { _sportsMode = mode; }
+    void useDefaultDatapage(bool useDefault) { _useDefaultDataPage = useDefault; }
+    void useCumulativeOperatingTime(bool useCumulativeOperatingTime) { _useCumulativeOperatingTime = useCumulativeOperatingTime; }
     void begin();
     void stop();
 protected:
@@ -22,14 +32,33 @@ private:
     void transmitPrimaryDataPage();
     void transmitBackgroundDataPage();
     void transmitRequestedDataPage();
+    void transmitHeartRateDefaultMsg();
+    void transmitHeartRateCumulativeOperatingTimeMsg();
+    void transmitHeartRateManufacturerInformationMsg();
+    void transmitHeartRateProductInformationMsg();
+    void transmitHeartRatePreviousHeartBeatMsg();
+    void transmitHeartRateSwimIntervalSummaryMsg();
+    void transmitHeartRateCapabilitiesMsg();
+    void transmitHeartRateBatteryStatusMsg();
+    void transmitHeartRateMsg(HeartRateBaseMainDataPageMsg& msg);
     bool handleModeSettings(HeartRateBaseMainDataPage& dataPage);
     bool handleRequestDataPage(HeartRateBaseMainDataPage& dataPage);
     Callback<RequestDataPage&> _onRequestDataPage;
     Callback<ModeSettings&> _onModeSettings;
-    Callback<HeartRateDefault&> _createHeartRateDefault;
-    Callback<HeartRateCapabilities&> _createHeartRateCapabilities;
-    Callback<HeartRateBatteryStatus&> _createHeartRateBatteryStatus;
+    Callback<HeartRateDefaultMsg&> _createHeartRateDefaultMsg;
+    Callback<HeartRateCumulativeOperatingTimeMsg&> _createHeartRateCumulativeOperatingTimeMsg;
+    Callback<HeartRateManufacturerInformationMsg&> _createHeartRateManufacturerInformationMsg;
+    Callback<HeartRateProductInformationMsg&> _createHeartRateProductInformationMsg;
+    Callback<HeartRatePreviousHeartBeatMsg&> _createHeartRatePreviousHeartBeatMsg;
+    Callback<HeartRateSwimIntervalSummaryMsg&> _createHeartRateSwimIntervalSummaryMsg;
+    Callback<HeartRateCapabilitiesMsg&> _createHeartRateCapabilitiesMsg;
+    Callback<HeartRateBatteryStatusMsg&> _createHeartRateBatteryStatusMsg;
     uint8_t _requestedCount = 0;
+    uint8_t _requestedPage;
+    uint8_t _nextBackgroundPage;
+    uint8_t _sportsMode = ANTPLUS_COMMON_DATAPAGE_MODESETTINGS_SPORTSMODE_RUNNING;
+    bool _useDefaultDataPage = false;
+    bool _useCumulativeOperatingTime = false;
 };
 
 #endif // ANTPLUS_PROFILEHEARTRATESENSOR_h
