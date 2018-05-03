dba996055ce1000249d8913ca8233f2868ddc827
Curtis Malainey <curtis@malainey.com>
2018-05-03 17:13:39 -0600

    Correct header guards

--- src/Profiles/BicycleSpeed/Sensor/ANTPLUS_ProfileBicycleSpeedSensor.h
+++ src/Profiles/BicycleSpeed/Sensor/ANTPLUS_ProfileBicycleSpeedSensor.h
@@ -1,25 +1,25 @@
-#ifndef ANTPLUS_PROFILEHEARTRATESENSOR_h
-#define ANTPLUS_PROFILEHEARTRATESENSOR_h
+#ifndef ANTPLUS_PROFILEBICYCLESPEEDSENSOR_h
+#define ANTPLUS_PROFILEBICYCLESPEEDSENSOR_h
 
 #include <BaseClasses/ANTPLUS_BaseMasterProfile.h>
-#include <Profiles/HeartRate/DataPages/ANTPLUS_ProfileHeartRateDataPages.h>
+#include <Profiles/BicycleSpeed/DataPages/ANTPLUS_ProfileBicycleSpeedDataPages.h>
 #include <CommonDataPages/ANTPLUS_CommonDataPages.h>
 
-class ProfileHeartRateMonitor : BaseMasterProfile {
+class ProfileBicycleSpeedSensor : BaseMasterProfile {
 public:
-    void onHeartRateCapabilities(void (*func)(HeartRateCapabilities&, uintptr_t), uintptr_t data = 0) { _onHeartRateCapabilities.set(func, data); }
-    void onRequestDataPage(void (*func)(RequestDataPage&, uintptr_t), uintptr_t data = 0) { _onRequestDataPage.set(func, data); }
-    void begin();
-    void stop();
-protected:
-    void onAcknowledgedData(AcknowledgedData& msg);
-    void onBroadcastData(BroadcastData& msg);
-    void transmitNextDataPage();
-private:
-    bool handleCapabilities(HeartRateBaseMainDataPage& dataPage);
-    bool handleRequestDataPage(HeartRateBaseMainDataPage& dataPage);
-    Callback<HeartRateCapabilities&> _onHeartRateCapabilities;
-    Callback<RequestDataPage&> _onRequestDataPage;
+//     void onHeartRateCapabilities(void (*func)(HeartRateCapabilities&, uintptr_t), uintptr_t data = 0) { _onHeartRateCapabilities.set(func, data); }
+//     void onRequestDataPage(void (*func)(RequestDataPage&, uintptr_t), uintptr_t data = 0) { _onRequestDataPage.set(func, data); }
+//     void begin();
+//     void stop();
+// protected:
+//     void onAcknowledgedData(AcknowledgedData& msg);
+//     void onBroadcastData(BroadcastData& msg);
+//     void transmitNextDataPage();
+// private:
+//     bool handleCapabilities(HeartRateBaseMainDataPage& dataPage);
+//     bool handleRequestDataPage(HeartRateBaseMainDataPage& dataPage);
+//     Callback<HeartRateCapabilities&> _onHeartRateCapabilities;
+//     Callback<RequestDataPage&> _onRequestDataPage;
 };
 
-#endif // ANTPLUS_PROFILEHEARTRATESENSOR_h
+#endif // ANTPLUS_PROFILEBICYCLESPEEDSENSOR_h
