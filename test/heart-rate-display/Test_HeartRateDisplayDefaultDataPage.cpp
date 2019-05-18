d9b097f51bf911425743182cce623f5b8c6023f8
Curtis Malainey <curtis@malainey.com>
2019-05-18 12:55:44 -0700

    Finished base HeartRate tests

--- test/heart-rate-display/Test_HeartRateDisplayDefaultDataPage.cpp
+++ test/heart-rate-display/Test_HeartRateDisplayDefaultDataPage.cpp
@@ -4,6 +4,11 @@
 
 #ifdef UNIT_TEST
 
+/**
+ * This tests the functionlity of the base page since
+ * it has no specific implementation details
+ */
+
 uint8_t mockedInput[] = {0, 0x80, 0xFF, 0xFF, 0xFF, 0xAB, 0xCD, 0x12, 0x45};
 HeartRateDefault *dp;
 
@@ -15,6 +20,18 @@ void test_pageChangeToggle(void) {
     TEST_ASSERT_EQUAL_UINT8(1, dp->getPageChangeToggle());
 }
 
+void test_heartBeatEventTime(void) {
+    TEST_ASSERT_EQUAL_UINT16(0xCDAB, dp->getHeartBeatEventTime());
+}
+
+void test_heartBeatCount(void) {
+    TEST_ASSERT_EQUAL_UINT8(0x12, dp->getHeartBeatCount());
+}
+
+void test_computerHeartRate(void) {
+    TEST_ASSERT_EQUAL_UINT8(0x45, dp->getComputedHeartRate());
+}
+
 int main(int argc, char **argv) {
     BroadcastData bd;
     bd.setFrameData(mockedInput);
@@ -24,6 +41,9 @@ int main(int argc, char **argv) {
     UNITY_BEGIN();
     RUN_TEST(test_datapage);
     RUN_TEST(test_pageChangeToggle);
+    RUN_TEST(test_heartBeatEventTime);
+    RUN_TEST(test_heartBeatCount);
+    RUN_TEST(test_computerHeartRate);
     UNITY_END();
 
     return 0;
