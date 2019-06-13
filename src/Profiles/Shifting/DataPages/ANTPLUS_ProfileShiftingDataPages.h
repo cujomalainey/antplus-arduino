d1cc986ba8e2276001322ebeabac3b7150dd8718
Sepp62 <33748087+Sepp62@users.noreply.github.com>
2019-06-13 23:21:33 +0200

    Shifting: First shot, not yet tested master channel
    Signed-off-by: Curtis Malainey <curtis@malainey.com>

---
+++ src/Profiles/Shifting/DataPages/ANTPLUS_ProfileShiftingDataPages.h
@@ -0,0 +1,13 @@
+#ifndef ANTPLUS_PROFILESHIFTINGDATAPAGES_h
+#define ANTPLUS_PROFILESHIFTINGDATAPAGES_h
+
+/* Base */
+#include <Profiles/Shifting/DataPages/Base/ANTPLUS_ShiftingBaseMainDataPageMsg.h>
+
+/* RX */
+// ...
+
+/* TX */
+// ...
+
+#endif // ANTPLUS_PROFILESHIFTINGDATAPAGES_h
