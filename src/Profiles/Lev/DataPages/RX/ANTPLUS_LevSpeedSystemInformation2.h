5e3ab2ed79c28c709b647077de4db2c6b529c3f1
Curtis Malainey <curtis@malainey.com>
2019-06-30 23:19:08 -0700

    Lev: DataPages: Remove dead declarations
    Moved to common base class

--- src/Profiles/Lev/DataPages/RX/ANTPLUS_LevSpeedSystemInformation2.h
+++ src/Profiles/Lev/DataPages/RX/ANTPLUS_LevSpeedSystemInformation2.h
@@ -8,11 +8,7 @@ public:
     LevSpeedSystemInformation2(AntRxDataResponse& dp);
     uint8_t getBatterySOC();
     uint8_t getBatteryEmptyWarning();
-    uint8_t getTravelModeState();
-    uint8_t getSystemState();
-    uint8_t getGearState();
     uint8_t getPercentAssist();
-    uint16_t getSpeed(); // in 1/10 km/h
 };
 
 #endif // ANTPLUS_LEVSPEEDSYSTEMINFORMATION2_h
