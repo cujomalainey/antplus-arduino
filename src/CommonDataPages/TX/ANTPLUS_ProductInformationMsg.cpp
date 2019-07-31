f79b900a967358061e0340a0b09ea6588bb598fd
Curtis Malainey <curtis@malainey.com>
2019-07-31 11:08:28 -0600

    CommonDataPages: Refactor: add productinformation mixin
    de-dup code and removed some magic numbers

--- src/CommonDataPages/TX/ANTPLUS_ProductInformationMsg.cpp
+++ src/CommonDataPages/TX/ANTPLUS_ProductInformationMsg.cpp
@@ -1,43 +1,25 @@
 #include <CommonDataPages/TX/ANTPLUS_ProductInformationMsg.h>
 #include <CommonDataPages/ANTPLUS_CommonDataPagePrivateDefines.h>
-#include <CommonDataPages/ANTPLUS_CommonDataPageDefines.h>
-#include <ANTPLUS_PrivateDefines.h>
 
-ProductInformationMsg::ProductInformationMsg() : BaseDataPageMsg() {
+ProductInformationMsg::ProductInformationMsg() : BaseDataPageMsg(), BaseProductInformation<BroadcastDataMsg>() {
     // TODO set default values
     setDataBuffer(_buffer);
-    _buffer[0] = 81;
+    _buffer[0] = ANTPLUS_COMMON_DATAPAGE_PRODUCTINFORMATION_NUMBER;
     _buffer[1] = 0xFF;
 }
 
-// TODO remove magic numbers
-uint8_t ProductInformationMsg::getSWRevisionSupplemental() {
-    return _buffer[2];
-}
-
-uint8_t ProductInformationMsg::getSWRevisionMain() {
-    return _buffer[3];
-}
-
-uint32_t ProductInformationMsg::getSerialNumber() {
-    uint32_t temp = _buffer[7] << 24;
-    temp |= _buffer[6] << 16;
-    temp |= _buffer[5] << 8;
-    temp |= _buffer[4];
-    return temp;
-}
-
 void ProductInformationMsg::setSWRevisionSupplemental(uint8_t revision) {
-    _buffer[2] = revision;
+    set8BitValue(revision,
+            ANTPLUS_COMMON_DATAPAGE_PRODUCTINFORMATION_SWREVISIONSUPPLEMENTAL_BYTE);
 }
 
 void ProductInformationMsg::setSWRevisionMain(uint8_t revision) {
-    _buffer[3] = revision;
+    set8BitValue(revision,
+            ANTPLUS_COMMON_DATAPAGE_PRODUCTINFORMATION_SWREVISIONMAIN_BYTE);
 }
 
 void ProductInformationMsg::setSerialNumber(uint32_t serialNumber) {
-    _buffer[7] = (serialNumber >> 24) & 0xFF;
-    _buffer[6] = (serialNumber >> 16) & 0xFF;
-    _buffer[5] = (serialNumber >> 8) & 0xFF;
-    _buffer[4] = serialNumber & 0xFF;
+    set32BitValue(serialNumber,
+            ANTPLUS_COMMON_DATAPAGE_PRODUCTINFORMATION_SERIALNUMBER_LSB_BYTE,
+            ANTPLUS_COMMON_DATAPAGE_PRODUCTINFORMATION_SERIALNUMBER_MSB_BYTE);
 }
--- src/CommonDataPages/TX/ANTPLUS_ProductInformationMsg.h
+++ src/CommonDataPages/TX/ANTPLUS_ProductInformationMsg.h
@@ -2,17 +2,15 @@
 #define ANTPLUS_PRODUCTINFORMATIONMSG_h
 
 #include <BaseClasses/ANTPLUS_BaseDataPageMsg.h>
+#include <CommonDataPages/Base/ANTPLUS_BaseProductInformation.h>
 
 #include "ANT.h"
 
 // Common page 81
 
-class ProductInformationMsg : public BaseDataPageMsg<BroadcastDataMsg> {
+class ProductInformationMsg : public BaseDataPageMsg<BroadcastDataMsg>, public BaseProductInformation<BroadcastDataMsg> {
 public:
     ProductInformationMsg();
-    uint8_t getSWRevisionSupplemental();
-    uint8_t getSWRevisionMain();
-    uint32_t getSerialNumber();
     void setSWRevisionSupplemental(uint8_t revision);
     void setSWRevisionMain(uint8_t revision);
     void setSerialNumber(uint32_t serialNumber);
