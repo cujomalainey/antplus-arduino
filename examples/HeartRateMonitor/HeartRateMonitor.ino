dd546e280ffd66a4b53399498b1542fc1a595e9f
Curtis Malainey <curtis@malainey.com>
2019-07-31 22:46:42 -0600

    HeartRate: Monitor: fixup example
    cleanup and make a minimum viable example

--- examples/HeartRateMonitor/HeartRateMonitor.ino
+++ examples/HeartRateMonitor/HeartRateMonitor.ino
@@ -5,6 +5,14 @@
  * mocked data to it and then reports
  * all events through the serial port
  *
+ * This is a minimum viable example
+ * all additional datapages are optional
+ * and are enabled through flags passed
+ * into the profile
+ *
+ * It is highly recommended you read the documenation at
+ * https://github.com/cujomalainey/antplus-arduino/wiki/HeartRate-Monitor-Profile
+ *
  * Author Curtis Malainey
  ************************************/
 #include <Arduino.h>
@@ -16,46 +24,44 @@
 
 const uint8_t NETWORK_KEY[] = {0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77}; // get this from thisisant.com
 
+uint32_t flags = 0;
 AntWithCallbacks ant;
 AntPlusRouter router;
-ProfileHeartRateMonitor hr(123, 0);
+ProfileHeartRateMonitor hr(123, 0, flags);
 
-void batteryStatusDataPageHandler(HeartRateBatteryStatusMsg& msg, uintptr_t data);
-void capabilitiesDataPageHandler(HeartRateCapabilitiesMsg& msg, uintptr_t data);
-void cumulativeOperatingTimeDataPageHandler(HeartRateCumulativeOperatingTimeMsg& msg, uintptr_t data);
+// void batteryStatusDataPageHandler(HeartRateBatteryStatusMsg& msg, uintptr_t data);
+// void capabilitiesDataPageHandler(HeartRateCapabilitiesMsg& msg, uintptr_t data);
+// void cumulativeOperatingTimeDataPageHandler(HeartRateCumulativeOperatingTimeMsg& msg, uintptr_t data);
 void defaultDataPageHandler(HeartRateDefaultMsg& msg, uintptr_t data);
 void manufacturerInformationDataPageHandler(HeartRateManufacturerInformationMsg& msg, uintptr_t data);
-void previousHeartBeatDataPageHandler(HeartRatePreviousHeartBeatMsg& msg, uintptr_t data);
+// void previousHeartBeatDataPageHandler(HeartRatePreviousHeartBeatMsg& msg, uintptr_t data);
 void productInformationDataPageHandler(HeartRateProductInformationMsg& msg, uintptr_t data);
-void swimIntervalSummary(HeartRateSwimIntervalSummaryMsg& msg, uintptr_t data);
+// void swimIntervalSummary(HeartRateSwimIntervalSummaryMsg& msg, uintptr_t data);
 
-void heartRateFeatures(uint8_t bitfield);
-void printStatus(uint8_t status);
+void populateBaseHeartRate(HeartRateBaseMainDataPageMsg& msg);
 
 void setup() {
     Serial1.begin(BAUD_RATE);
     ant.setSerial(Serial1);
     delay(15000);
+    Serial.begin(BAUD_RATE);
+    Serial.println("Running");
 
     router.setDriver(&ant); // never touch ant again
     router.setAntPlusNetworkKey(NETWORK_KEY);
     router.setProfile(CHANNEL_0, &hr);
     // Delay after initial setup to wait for user to connect on serial
 
-    Serial.begin(BAUD_RATE);
-    Serial.println("Running");
     hr.createHeartRateBatteryStatusMsg(batteryStatusDataPageHandler);
-    hr.createHeartRateCapabilitiesMsg(capabilitiesDataPageHandler);
-    hr.createHeartRateCumulativeOperatingTimeMsg(cumulativeOperatingTimeDataPageHandler);
+    // hr.createHeartRateCapabilitiesMsg(capabilitiesDataPageHandler);
+    // hr.createHeartRateCumulativeOperatingTimeMsg(cumulativeOperatingTimeDataPageHandler);
     hr.createHeartRateDefaultMsg(defaultDataPageHandler);
     hr.createHeartRateManufacturerInformationMsg(manufacturerInformationDataPageHandler);
-    hr.createHeartRatePreviousHeartBeatMsg(previousHeartBeatDataPageHandler);
+    // hr.createHeartRatePreviousHeartBeatMsg(previousHeartBeatDataPageHandler);
     hr.createHeartRateProductInformationMsg(productInformationDataPageHandler);
-    hr.createHeartRateSwimIntervalSummaryMsg(swimIntervalSummary);
-    hr.begin();
-    // print channel status
+    // hr.createHeartRateSwimIntervalSummaryMsg(swimIntervalSummary);
     Serial.println("===========================");
-    /* printStatus(status); */
+    hr.begin();
 }
 
 void loop() {
@@ -70,53 +76,69 @@ void printDpMsg(int dp, const char* s) {
     Serial.println(s);
 }
 
-void batteryStatusDataPageHandler(HeartRateBatteryStatusMsg& msg, uintptr_t data) {
-    printDpMsg(7, "Battery Status");
-}
+/* Optional */
+// void batteryStatusDataPageHandler(HeartRateBatteryStatusMsg& msg, uintptr_t data) {
+//     printDpMsg(7, "Battery Status");
+//     populateBaseHeartRate(msg);
+// }
 
-void capabilitiesDataPageHandler(HeartRateCapabilitiesMsg& msg, uintptr_t data) {
-    printDpMsg(6, "Capabilities");
-}
+/* Optional */
+// void capabilitiesDataPageHandler(HeartRateCapabilitiesMsg& msg, uintptr_t data) {
+//     printDpMsg(6, "Capabilities");
+//     populateBaseHeartRate(msg);
+// }
 
-void cumulativeOperatingTimeDataPageHandler(HeartRateCumulativeOperatingTimeMsg& msg, uintptr_t data) {
-    printDpMsg(1, "Cumulative Operating Time");
-}
+/* Optional */
+// void cumulativeOperatingTimeDataPageHandler(HeartRateCumulativeOperatingTimeMsg& msg, uintptr_t data) {
+//     printDpMsg(1, "Cumulative Operating Time");
+//     populateBaseHeartRate(msg);
+// }
 
 void defaultDataPageHandler(HeartRateDefaultMsg& msg, uintptr_t data) {
     // All fields are reserved
     printDpMsg(0, "Default");
+    populateBaseHeartRate(msg);
 }
 
 void manufacturerInformationDataPageHandler(HeartRateManufacturerInformationMsg& msg, uintptr_t data) {
     printDpMsg(2, "Manufacturer Information");
+    populateBaseHeartRate(msg);
+    msg.setManufacturerIdLsb(78);
+    msg.setSerialNumber(0xabcd);
 }
 
-void previousHeartBeatDataPageHandler(HeartRatePreviousHeartBeatMsg& dp, uintptr_t data) {
-    printDpMsg(4, "Previous Heart Beat");
-}
+/* Optional */
+// void previousHeartBeatDataPageHandler(HeartRatePreviousHeartBeatMsg& msg, uintptr_t data) {
+//     printDpMsg(4, "Previous Heart Beat");
+//     populateBaseHeartRate(msg);
+// }
 
 void productInformationDataPageHandler(HeartRateProductInformationMsg& msg, uintptr_t data) {
     printDpMsg(3, "Product Information");
+    populateBaseHeartRate(msg);
+    msg.setHardwareVersion(1);
+    msg.setSoftwareVersion(2);
+    msg.setModelNumber(3);
 }
 
-void swimIntervalSummary(HeartRateSwimIntervalSummaryMsg& msg, uintptr_t data) {
-    printDpMsg(5, "Swim Interval");
-}
+/* Optional */
+// void swimIntervalSummary(HeartRateSwimIntervalSummaryMsg& msg, uintptr_t data) {
+//     printDpMsg(5, "Swim Interval");
+//     populateBaseHeartRate(msg);
+// }
 
-void printStatus(uint8_t status) {
-    Serial.print("Channel Status: ");
-    switch (status) {
-    case CHANNEL_STATUS_UNASSIGNED:
-        Serial.println("Unassigned");
-        break;
-    case CHANNEL_STATUS_ASSIGNED:
-        Serial.println("Assigned");
-        break;
-    case CHANNEL_STATUS_SEARCHING:
-        Serial.println("Searching");
-        break;
-    case CHANNEL_STATUS_TRACKING:
-        Serial.println("Tracking");
-        break;
+void populateBaseHeartRate(HeartRateBaseMainDataPageMsg& msg) {
+    static uint8_t toggle = 0;
+    static uint8_t hr = 0;
+    static uint16_t eventTime = 0;
+    static uint8_t count = 0;
+    msg.setPageChangeToggle(toggle++ < 4);
+    msg.setComputedHeartRate(20*sin(hr++) + 120);
+    msg.setHeartBeatEventTime(eventTime);
+    msg.setHeartBeatCount(count++);
+
+    if (toggle >= 8) {
+        toggle = 0;
     }
+    eventTime += 120;
 }
