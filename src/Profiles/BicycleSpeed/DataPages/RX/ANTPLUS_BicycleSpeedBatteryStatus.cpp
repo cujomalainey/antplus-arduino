f60a2be0fff92780643d73dab65af30878b6349c
Curtis Malainey <curtis@malainey.com>
2019-09-08 16:00:26 -0700

    BicycleSpeed: DataPages: BatteryStatus refactor
    Move to new data API, preps for sensor profile

--- src/Profiles/BicycleSpeed/DataPages/RX/ANTPLUS_BicycleSpeedBatteryStatus.cpp
+++ src/Profiles/BicycleSpeed/DataPages/RX/ANTPLUS_BicycleSpeedBatteryStatus.cpp
@@ -1,18 +1,5 @@
 #include <Profiles/BicycleSpeed/DataPages/RX/ANTPLUS_BicycleSpeedBatteryStatus.h>
 #include <Profiles/BicycleSpeed/ANTPLUS_BicycleSpeedPrivateDefines.h>
 
-BicycleSpeedBatteryStatus::BicycleSpeedBatteryStatus(AntRxDataResponse& dp) : BicycleSpeedBaseMainDataPage(dp) {
-
-}
-
-uint8_t BicycleSpeedBatteryStatus::getFractionalBatteryVoltage() {
-    return getData(ANTPLUS_BICYCLESPEED_DATAPAGE_BATTERYSTATUS_FRACTIONALBATTERYVOLTAGE_BYTE);
-}
-
-uint8_t BicycleSpeedBatteryStatus::getCoarseBatteryVoltage() {
-    return getData(ANTPLUS_BICYCLESPEED_DATAPAGE_BATTERYSTATUS_COARSEBATTERYVOLTAGE_BYTE) & ANTPLUS_BICYCLESPEED_DATAPAGE_BATTERYSTATUS_COARSEBATTERYVOLTAGE_MASK;
-}
-
-uint8_t BicycleSpeedBatteryStatus::getBatteryStatus() {
-    return (getData(ANTPLUS_BICYCLESPEED_DATAPAGE_BATTERYSTATUS_BATTERYSTATUS_BYTE) & ANTPLUS_BICYCLESPEED_DATAPAGE_BATTERYSTATUS_BATTERYSTATUS_MASK) >> ANTPLUS_BICYCLESPEED_DATAPAGE_BATTERYSTATUS_BATTERYSTATUS_SHIFT;
+BicycleSpeedBatteryStatus::BicycleSpeedBatteryStatus(AntRxDataResponse& dp) : BicycleSpeedBaseMainDataPage(dp), BicycleSpeedBaseBatteryStatus<BroadcastData>() {
 }
