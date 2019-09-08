ce2096cf145af980cf66510ce01ca93c065e0a2f
Curtis Malainey <curtis@malainey.com>
2019-09-08 16:40:45 -0700

    BicycleSpeed: DataPages: BatteryStatus remove unneeded include
    Save some compile time

--- src/Profiles/BicycleSpeed/DataPages/RX/ANTPLUS_BicycleSpeedBatteryStatus.cpp
+++ src/Profiles/BicycleSpeed/DataPages/RX/ANTPLUS_BicycleSpeedBatteryStatus.cpp
@@ -1,5 +1,4 @@
 #include <Profiles/BicycleSpeed/DataPages/RX/ANTPLUS_BicycleSpeedBatteryStatus.h>
-#include <Profiles/BicycleSpeed/ANTPLUS_BicycleSpeedPrivateDefines.h>
 
 BicycleSpeedBatteryStatus::BicycleSpeedBatteryStatus(AntRxDataResponse& dp) : BicycleSpeedBaseMainDataPage(dp), BicycleSpeedBaseBatteryStatus<BroadcastData>() {
 }
