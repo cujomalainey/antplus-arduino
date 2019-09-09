371aa4251c7046f427d45463436cb452b1ecc47e
Curtis Malainey <curtis@malainey.com>
2019-09-08 23:02:28 -0700

    BicycleSpeed: DataPages: Remove unneeded includes
    not needed anymore, speed up things

--- src/Profiles/BicycleSpeed/DataPages/RX/ANTPLUS_BicycleSpeedManufacturerID.cpp
+++ src/Profiles/BicycleSpeed/DataPages/RX/ANTPLUS_BicycleSpeedManufacturerID.cpp
@@ -1,5 +1,4 @@
 #include <Profiles/BicycleSpeed/DataPages/RX/ANTPLUS_BicycleSpeedManufacturerID.h>
-#include <Profiles/BicycleSpeed/ANTPLUS_BicycleSpeedPrivateDefines.h>
 
 BicycleSpeedManufacturerID::BicycleSpeedManufacturerID(AntRxDataResponse& dp) : BicycleSpeedBaseMainDataPage(dp), BicycleSpeedBaseManufacturerID<BroadcastData>() {
 }
--- src/Profiles/BicycleSpeed/DataPages/RX/ANTPLUS_BicycleSpeedMotionAndSpeed.cpp
+++ src/Profiles/BicycleSpeed/DataPages/RX/ANTPLUS_BicycleSpeedMotionAndSpeed.cpp
@@ -1,5 +1,4 @@
 #include <Profiles/BicycleSpeed/DataPages/RX/ANTPLUS_BicycleSpeedMotionAndSpeed.h>
-#include <Profiles/BicycleSpeed/ANTPLUS_BicycleSpeedPrivateDefines.h>
 
 BicycleSpeedMotionAndSpeed::BicycleSpeedMotionAndSpeed(AntRxDataResponse& dp) : BicycleSpeedBaseMainDataPage(dp) {
 }
