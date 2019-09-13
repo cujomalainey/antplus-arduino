10d039dfcb81d05367b02e9689734a068bdf91c0
Curtis Malainey <curtis@malainey.com>
2019-09-13 09:40:28 -0700

    Fix workaround
    need reinterpret cast not static cast, oops

--- src/MainClasses/ANTPLUS_CallbackWorkAround.cpp
+++ src/MainClasses/ANTPLUS_CallbackWorkAround.cpp
@@ -13,76 +13,76 @@
  */
 
 void onPacketErrorCallback(uint8_t error, uintptr_t data) {
-    AntPlusRouter* router = static_cast<AntPlusRouter*>(data);
+    AntPlusRouter* router = reinterpret_cast<AntPlusRouter*>(data);
     router->onPacketError(error);
 }
 
 void onAcknowledgedDataCallback(AcknowledgedData& msg, uintptr_t data) {
-    AntPlusRouter* router = static_cast<AntPlusRouter*>(data);
+    AntPlusRouter* router = reinterpret_cast<AntPlusRouter*>(data);
     router->onAcknowledgedData(msg);
 }
 
 void onAdvancedBurstDataCallback(AdvancedBurstData& msg, uintptr_t data) {
-    AntPlusRouter* router = static_cast<AntPlusRouter*>(data);
+    AntPlusRouter* router = reinterpret_cast<AntPlusRouter*>(data);
     router->onAdvancedBurstData(msg);
 }
 
 void onBroadcastDataCallback(BroadcastData& msg, uintptr_t data) {
-    AntPlusRouter* router = static_cast<AntPlusRouter*>(data);
+    AntPlusRouter* router = reinterpret_cast<AntPlusRouter*>(data);
     router->onBroadcastData(msg);
 }
 
 void onBurstTransferDataCallback(BurstTransferData& msg, uintptr_t data) {
-    AntPlusRouter* router = static_cast<AntPlusRouter*>(data);
+    AntPlusRouter* router = reinterpret_cast<AntPlusRouter*>(data);
     router->onBurstTransferData(msg);
 }
 
 void onAdvancedBurstCapabilitiesConfigurationCallback(AdvancedBurstCapabilitiesConfiguration& msg, uintptr_t data) {
-    AntPlusRouter* router = static_cast<AntPlusRouter*>(data);
+    AntPlusRouter* router = reinterpret_cast<AntPlusRouter*>(data);
     router->onAdvancedBurstCapabilitiesConfiguration(msg);
 }
 
 void onAntVersionCallback(AntVersion& msg, uintptr_t data) {
-    AntPlusRouter* router = static_cast<AntPlusRouter*>(data);
+    AntPlusRouter* router = reinterpret_cast<AntPlusRouter*>(data);
     router->onAntVersion(msg);
 }
 
 void onCapabilitiesCallback(Capabilities& msg, uintptr_t data) {
-    AntPlusRouter* router = static_cast<AntPlusRouter*>(data);
+    AntPlusRouter* router = reinterpret_cast<AntPlusRouter*>(data);
     router->onCapabilities(msg);
 }
 
 void onChannelEventResponseCallback(ChannelEventResponse& msg, uintptr_t data) {
-    AntPlusRouter* router = static_cast<AntPlusRouter*>(data);
+    AntPlusRouter* router = reinterpret_cast<AntPlusRouter*>(data);
     router->onChannelEventResponse(msg);
 }
 
 void onChannelIdResponseCallback(ChannelIdResponse& msg, uintptr_t data) {
-    AntPlusRouter* router = static_cast<AntPlusRouter*>(data);
+    AntPlusRouter* router = reinterpret_cast<AntPlusRouter*>(data);
     router->onChannelIdResponse(msg);
 }
 
 void onChannelStatusCallback(ChannelStatus& msg, uintptr_t data) {
-    AntPlusRouter* router = static_cast<AntPlusRouter*>(data);
+    AntPlusRouter* router = reinterpret_cast<AntPlusRouter*>(data);
     router->onChannelStatus(msg);
 }
 
 void onEncryptionModeParametersCallback(EncryptionModeParameters& msg, uintptr_t data) {
-    AntPlusRouter* router = static_cast<AntPlusRouter*>(data);
+    AntPlusRouter* router = reinterpret_cast<AntPlusRouter*>(data);
     router->onEncryptionModeParameters(msg);
 }
 
 void onEventFilterCallback(EventFilter& msg, uintptr_t data) {
-    AntPlusRouter* router = static_cast<AntPlusRouter*>(data);
+    AntPlusRouter* router = reinterpret_cast<AntPlusRouter*>(data);
     router->onEventFilter(msg);
 }
 
 void onSelectiveDataUpdateMaskSettingCallback(SelectiveDataUpdateMaskSetting& msg, uintptr_t data) {
-    AntPlusRouter* router = static_cast<AntPlusRouter*>(data);
+    AntPlusRouter* router = reinterpret_cast<AntPlusRouter*>(data);
     router->onSelectiveDataUpdateMaskSetting(msg);
 }
 
 void onStartUpMessageCallback(StartUpMessage& msg, uintptr_t data) {
-    AntPlusRouter* router = static_cast<AntPlusRouter*>(data);
+    AntPlusRouter* router = reinterpret_cast<AntPlusRouter*>(data);
     router->onStartUpMessage(msg);
 }
