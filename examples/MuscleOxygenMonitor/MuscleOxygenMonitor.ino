900b225e74041ce2a9b3ec1656d6db51543e7788
Curtis Malainey <curtis@malainey.com>
2021-01-13 11:16:27 -0800

    moxy: fixup monitor example with new ant-arduino types
    also fix a trailing whitespace

--- examples/MuscleOxygenMonitor/MuscleOxygenMonitor.ino
+++ examples/MuscleOxygenMonitor/MuscleOxygenMonitor.ino
@@ -16,9 +16,9 @@
 
 const uint8_t NETWORK_KEY[] = { 0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77 };
 
-AntWithCallbacks ant = AntWithCallbacks();
-AntPlusRouter router = AntPlusRouter();
-ProfileMuscleOxygenMonitor moxy = ProfileMuscleOxygenMonitor( 7369 );
+ArduinoSerialAntWithCallbacks ant;
+AntPlusRouter router;
+ProfileMuscleOxygenMonitor moxy(123);
 
 void moxyCreateMsgHandler(MuscleOxygenBaseMainDataPageMsg& msg, uintptr_t data);
 void moxyCreateManufacturerInformationMsg(ManufacturersInformationMsg& msg, uintptr_t data);
