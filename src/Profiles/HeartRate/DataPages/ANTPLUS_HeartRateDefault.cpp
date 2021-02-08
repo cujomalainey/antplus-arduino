d2bf2d1e5aa6ace249903d8a4d6052a9c5167735
Curtis Malainey <curtis@malainey.com>
2021-02-07 16:25:59 -0800

    heartrate: fixup reserved set

--- src/Profiles/HeartRate/DataPages/ANTPLUS_HeartRateDefault.cpp
+++ src/Profiles/HeartRate/DataPages/ANTPLUS_HeartRateDefault.cpp
@@ -1,13 +1,14 @@
 #include <Profiles/HeartRate/DataPages/ANTPLUS_HeartRateDefault.h>
 #include <Profiles/HeartRate/ANTPLUS_HeartRatePrivateDefines.h>
 
+#define RESERVED_LSB_BYTE 1
+#define RESERVED_MSB_BYTE 3
+#define RESERVED_VALUE 0xFFFFFF
+
 HeartRateDefault::HeartRateDefault(AntRxDataResponse& dp) :
     HeartRateBaseMainDataPage(dp) {}
 
 HeartRateDefaultMsg::HeartRateDefaultMsg() :
     HeartRateBaseMainDataPageMsg(HEARTRATE_DEFAULT_NUMBER) {
-    // TODO magic numbers
-    _buffer[1] = 0xFF;
-    _buffer[2] = 0xFF;
-    _buffer[3] = 0xFF;
+    set24BitValue(RESERVED_VALUE, RESERVED_LSB_BYTE, RESERVED_MSB_BYTE);
 }
