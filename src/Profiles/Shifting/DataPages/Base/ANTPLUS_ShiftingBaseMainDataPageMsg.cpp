e44b0f199394b4687a4cab2dff077e27a3dee25b
Curtis Malainey <cujomalainey@chromium.org>
2021-02-07 17:44:38 -0800

    shifting: fixup magic nums and api calls

--- src/Profiles/Shifting/DataPages/Base/ANTPLUS_ShiftingBaseMainDataPageMsg.cpp
+++ src/Profiles/Shifting/DataPages/Base/ANTPLUS_ShiftingBaseMainDataPageMsg.cpp
@@ -2,11 +2,13 @@
 #include <Profiles/Shifting/ANTPLUS_ShiftingPrivateDefines.h>
 #include <ANTPLUS_PrivateDefines.h>
 
+#define RESERVED_BYTE 2
+#define RESERVED_VALUE 0xFF
+
 ShiftingBaseMainDataPageMsg::ShiftingBaseMainDataPageMsg(uint8_t dataPageNumber) : BaseDataPageMsg<BroadcastDataMsg>()
 {
     memset(_buffer, 0, MESSAGE_SIZE);
     setDataBuffer(_buffer);
     set8BitValue(dataPageNumber, ANTPLUS_SHIFTING_DATAPAGEBASE_DATAPAGE_BYTE);
-    // TODO magic numbers
-    _buffer[2] = 0xFF;
+    set8BitValue(RESERVED_VALUE, RESERVED_BYTE);
 }
