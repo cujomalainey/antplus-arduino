2ae4378a2c6cbf091f142949b57b38c1ee1f223c
Curtis Malainey <curtis@malainey.com>
2021-02-15 17:26:21 -0800

    MuscleOxygen: set reserver byte for command msg

--- src/Profiles/MuscleOxygen/DataPages/ANTPLUS_MuscleOxygenCommands.cpp
+++ src/Profiles/MuscleOxygen/DataPages/ANTPLUS_MuscleOxygenCommands.cpp
@@ -2,9 +2,12 @@
 #include <Profiles/MuscleOxygen/ANTPLUS_MuscleOxygenPrivateDefines.h>
 
 #define COMMANDID_BYTE 1
+#define RESERVED_BYTE 2
 #define LOCALTIMEOFFSET_BYTE 3
 #define CURRENTTIMETIMESTAMP_LSB_BYTE 4
 #define CURRENTTIMETIMESTAMP_MSB_BYTE 7
+
+#define RESERVED_VALUE 0xFF
 
 template<class T>
 MuscleOxygenBaseCommands<T>::MuscleOxygenBaseCommands() : CoreDataPage<T>() {}
@@ -34,7 +37,9 @@ MuscleOxygenCommands::MuscleOxygenCommands(AntRxDataResponse& dp) :
 
 MuscleOxygenCommandsMsg::MuscleOxygenCommandsMsg() :
     MuscleOxygenBaseMainDataPageMsg(MUSCLEOXYGEN_COMMANDS_NUMBER),
-    MuscleOxygenBaseCommands<BroadcastDataMsg>() {}
+    MuscleOxygenBaseCommands<BroadcastDataMsg>() {
+    set8BitValue(RESERVED_VALUE, RESERVED_BYTE);
+}
 
 void MuscleOxygenCommandsMsg::setCommandId(uint8_t id) {
     set8BitValue(id, COMMANDID_BYTE);
